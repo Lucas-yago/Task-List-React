41a436f234d2f6c84727a1baf55e3e2fc587a11c
Lucas-yago <41453379+Lucas-yago@users.noreply.github.com>
2021-10-09 15:33:16 -0300

    implementa alteraçao no valor do atributo done do item da lista de acordo com estado da checkBox

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,23 +1,20 @@
-
-import { useState } from 'react';
 import * as C from './styles';
 import{Item} from '../../types/item';
 
 type Props = {
     item: Item;
+    onChange: (id: number, done: boolean) => void;
 }
 
-export const ListItem = ({item}: Props) =>{
-
-    const[isChecked, setIsCheked] = useState(item.done);
+export const ListItem = ({item, onChange}: Props) =>{
 
     return (
-        <C.Container done={isChecked}>
+        <C.Container done={item.done}>
             <input type='checkbox'
-             checked={isChecked}
-             onChange={e=> setIsCheked(e.target.checked)}
+             checked={item.done}
+             onChange={e => onChange(item.id, e.target.checked)}
              />
-            <label>{item.name}</label>
+            <label>{item.name} {item.done.toString()}</label>
         </C.Container>
 
     )
